f709010f364a750fe8d9a9ed1a02d018b4214de0
Zakari Nazim SAIBI <saibi.nazim@gmail.com>
2017-12-22 10:25:14 +0100

    ESP init project

---
+++ ESP-mqtt/defines.hpp
@@ -0,0 +1,16 @@
+#ifndef _DEFINES_HPP_
+#define _DEFINES_HPP_
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "password"
+
+#define MQTT_SERVER "192.168.1.21"
+#define MQTT_TOPIC "test"
+#define MQTT_USER "user"
+#define MQTT_PASSWORD "password"
+
+#define LED_STATE 16
+
+#define SERIAL_SPEED 115200
+
+#endif
